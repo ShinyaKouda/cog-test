09ec1c1fc0e6514abf017c98c3c3fe99da4ff4ab
Shinya Kouda <kouda.shinya@tdse.jp>
2025-03-10 05:54:21 +0900

    Update focus-webchat.js

--- static-cognigy/focus-webchat.js
+++ static-cognigy/focus-webchat.js
@@ -1,14 +1,24 @@
 webchat.registerAnalyticsService(event => {
-
-    // メッセージを受信したときは、最後のユーザーのメッセージが一番上に来るようにする
+    // メッセージを受信したときは、ユーザーのメッセージがある場合は最後のユーザーのメッセージが一番上に、
+    // 無い場合は最初のボットのメッセージが一番上に来るようにする
     if (event.type === "webchat/incoming-message") {
         setTimeout(() => {
             var chatContainer = document.querySelector('.webchat-chat-history');
-            var elements = document.querySelectorAll('.regular-message.user');
-            var lastElement = elements[elements.length-1];
+            var userMessages = document.querySelectorAll('.regular-message.user');
+            var targetElement;
             
-            if (lastElement) {
-                var topPosition = lastElement.offsetTop - lastElement.offsetHeight; // 最後のメッセージの上端の位置を取得
+            if (userMessages.length > 0) {
+                // ユーザーメッセージがある場合は最後のユーザーメッセージを取得
+                targetElement = userMessages[userMessages.length - 1];
+            } else {
+                // ユーザーメッセージがない場合は最初のボットのメッセージを取得（.botクラスを仮定）
+                var botMessages = document.querySelectorAll('.regular-message.bot');
+                targetElement = botMessages[0];
+            }
+            
+            if (targetElement) {
+                // 要素の上端の位置を取得してスクロールする
+                var topPosition = targetElement.offsetTop - targetElement.offsetHeight;
                 chatContainer.scrollTop = topPosition;
             }
         }, 100);
